0737a54b78fa2dadff3e9c73d8f6cadc2f4a1b91
cazkol-create <gaymanplsstop@gmail.com>
2026-04-10 00:39:09 +0800

    Updated ServerScriptService/ToolHandler.lua

--- ServerScriptService/ToolHandler.lua
+++ ServerScriptService/ToolHandler.lua
@@ -2,14 +2,23 @@
 -- Server Script
 game.Players.PlayerAdded:Connect(function(plr)			
 	plr.CharacterAdded:Connect(function(char)		
-		local M6D = Instance.new("Motor6D") 
-		M6D.Name = "Motor6D"
-		M6D.Parent = char['Right Arm'] -- or the part that you have decieded
-		M6D.Part0 = char['Right Arm']
-
 		char.ChildAdded:Connect(function(child)
-			if child:IsA("Tool") and child:FindFirstChild("RightArmAttach") then
+			if not child:IsA("Tool")  then return end
+			
+			if child:FindFirstChild("RightArmAttach") then
+				local M6D = Instance.new("Motor6D") 
+				M6D.Name = "Motor6D"
+				M6D.Parent = char['Right Arm'] -- or the part that you have decieded
+				M6D.Part0 = char['Right Arm']
 				M6D.Part1 = child.RightArmAttach
+			end
+			
+			if child:FindFirstChild("BodyAttach") then
+				local M6D = Instance.new("Motor6D") 
+				M6D.Name = "Motor6D"
+				M6D.Parent = char['Torso'] -- or the part that you have decieded
+				M6D.Part0 = char['Torso']
+				M6D.Part1 = child.BodyAttach
 			end
 		end)
 	end)
