f790edcdfd352aa984d0b731e289fd432ec2db2b
cazkol-create <gaymanplsstop@gmail.com>
2026-04-11 23:08:16 +0800

    Updated StarterPlayer/StarterPlayerScripts/ClientSoundHandler.lua

--- StarterPlayer/StarterPlayerScripts/ClientSoundHandler.lua
+++ StarterPlayer/StarterPlayerScripts/ClientSoundHandler.lua
@@ -1,4 +1,7 @@
+-- @ScriptType: LocalScript
 local RS = game:GetService("ReplicatedStorage")
+local Debris = game:GetService("Debris")
+
 local SoundFX = RS:WaitForChild("SoundFX")
 local SoundsFolder = RS:WaitForChild("Sounds")
 
@@ -8,16 +11,25 @@ SoundFX.OnClientEvent:Connect(function(data)
 	local soundType = data.type
 	local position = data.position
 
-	local soundTemplate = SoundsFolder:FindFirstChild(soundType)
-	if not soundTemplate then
+	local template = SoundsFolder:FindFirstChild(soundType)
+	if not template then
 		warn("Missing sound:", soundType)
 		return
 	end
 
-	local sound = soundTemplate:Clone()
-	sound.Parent = workspace
-	sound.Position = position
+	local part = Instance.new("Part")
+	part.Anchored = true
+	part.CanCollide = false
+	part.CanQuery = false
+	part.CanTouch = false
+	part.Transparency = 1
+	part.Size = Vector3.new(1, 1, 1)
+	part.Position = position
+	part.Parent = workspace
 
+	local sound = template:Clone()
+	sound.Parent = part
 	sound:Play()
-	game:GetService("Debris"):AddItem(sound, sound.TimeLength + 1)
+
+	Debris:AddItem(part, math.max(sound.TimeLength + 1, 2))
 end)
